fb57fd95b7454da77099e1528ee41673102984ff
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2015-04-24 23:14:34 -0400

    Hover state on nav items

--- templates/header.php
+++ templates/header.php
@@ -4,10 +4,13 @@
 			<img data-navigate="home" class="logo" src="img/logo.png" alt="StreamYourAd Logo" width="172">
 			<nav>
 				<ul>
-					<li data-navigate="home">HOME</li><!-- Home nav button is selected by default on page load -->
-					<li data-navigate="about">HOW IT WORKS</li>
-					<li data-navigate="services">OUR SERVICES</li>
-					<li data-navigate="contact">CONTACT US</li>
+					<!--
+						Home nav button is selected by default on page load 
+					-->
+					<li data-navigate="home" class="SELECTED">HOME<span></span></li>
+					<li data-navigate="about">HOW IT WORKS<span></span></li>
+					<li data-navigate="services">OUR SERVICES<span></span></li>
+					<li data-navigate="contact">CONTACT US<span></span></li>
 				</ul>
 			</nav>
 		</div>
