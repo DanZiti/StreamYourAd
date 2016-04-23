4ab024efe1005f0cbbb21a7364699e27c7b87f49
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2016-04-22 22:33:06 -0600

    Further implement refactoring of each JS module

--- js/stream-your-ad.js
+++ js/stream-your-ad.js
@@ -6,7 +6,7 @@
 	// Gather elements
 	//
 	var collectDom = function() {
-		
+		// TODO: FILL THIS!!!
 	};
 	
 	// Navigation for desktop and mobile
@@ -70,18 +70,108 @@
 	};
 	
 	
-	// asdf
+	// Control slider animations
 	//
+	var controlSlider = function() {
+		
+		var index = 0;
+		var curHeight = w.innerHeight;
+		var slides = d.querySelectorAll("#slider .slide");
+		var list = d.getElementById("slider-bullets");
+		var bullets = d.querySelectorAll("#slider-bullets li");
+		
+		
+		$("#slider").height(initialHeight - 80); // -80 here accounts for adjustments in CSS for the sticky header
+		
+		$(w).resize(function() {
+			if ($(this).height() > curHeight) {
+				$("#slider").height(w.innerHeight - 80);
+				curHeight = w.innerHeight;
+			}
+		})
+		.on("load resize", function() {
+			if ($(this).width() <= 760) {
+				var minHeight = $("#responsive-slide").height() + 150;
+				$("#slider").css("min-height", minHeight + "px");
+			}
+		})
+		.on("orientationchange", function() {
+			
+			var newHeight = w.innerHeight - 80;
+			
+			$("#slider")
+				.height(newHeight)
+				.css("min-height", newHeight);
+		});
+		
+		// Build bullets
+		//
+		for (var i = 0; i < slides.length; i++) {
+			var bullet = d.createElement("li");
+			if (i === 0) bullet.className = "SELECTED";
+			list.appendChild(bullet);
+		}
+		
+		// Auto rotations
+		//
+		function rotate() {
+			
+			index++;
+			
+			if (index === slides.length) index = 0;
+			
+			sya
+				.hide("#slider .slide")
+				.show(slides[index])
+				.deselect("#slider-bullets li")
+				.select(bullets[index]);
+			
+			setTimeout(rotate, 8000); // Trigger rotation every 8 seconds recursively
+		};
+	};
 	
-	
-	
-	// asdf
+	// Form validation
 	//
-	
-	
-	
-	// asdf
-	//
+	var validateForm = function() {
+		
+		var form = d.forms["contact-form"];
+		var name = form["name"];
+		var email = form["email"];
+		
+		$(form).on("submit", function() {
+			
+			if (name.value === null || name.value === "") {
+			
+				alert("Hey there, looks like you didn't fill out your name...");
+				
+				name.className = "invalid";
+				name.focus();
+				
+				return false;
+				
+			} else {
+				name.className = "";
+			}
+			
+			if (!email.value.match("@")) {
+				
+				alert("Please provide a valid email address, e.g. john@doe.com");
+				
+				email.className = "invalid";
+				email.select();
+				email.focus();
+				
+				return false;
+				
+			} else {
+				email.className = "";
+			}
+			
+			alert("Thank you for your submission: someone will be in touch with you soon!");
+			
+		});
+		
+	};
 		
 	
 	/////////////////////////////////////////////////////////////////////
@@ -96,6 +186,8 @@
 		collectDom();
 		controlNavigation();
 		controlParallax();
+		controlSlider();
+		validateForm();
 	};
 	
 	$(d).ready(init);
