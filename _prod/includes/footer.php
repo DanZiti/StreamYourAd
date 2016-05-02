4689db48fd45689ef579eaaf68e3f77cffba38ba
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2016-05-02 16:53:18 -0600

    Update footer

--- _prod/includes/footer.php
+++ _prod/includes/footer.php
@@ -1,7 +1,7 @@
 <footer>
 	<ul>
 		<li>Privacy Policy</li>
-		<li>Terms & Conditions</li>
+		<li>Terms &amp; Conditions</li>
 		<li>&copy; <?php echo date('Y'); ?> StreamYourAd.com</li>
 	</ul>
 </footer>
