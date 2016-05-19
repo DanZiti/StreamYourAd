48cc8543a81ae09fb2b979ecda8954d5b7532db3
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2016-05-18 22:43:49 -0600

    Debug spacing error in footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer>
 	<ul>
-		<li>Privacy Policy</li>
-		<li>Terms &amp; Conditions</li>
-		<li>&copy; <?php echo date('Y'); ?> StreamYourAd.com</li>
+		<li>Privacy Policy</li><!--
+		--><li>Terms &amp; Conditions</li><!--
+		--><li>&copy; <?php echo date('Y'); ?> StreamYourAd.com</li>
 	</ul>
 </footer>
