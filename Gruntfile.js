ad1b40fc794ffdc44ddd94fec4d425c4ddd0e220
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2016-08-16 18:55:31 -0600

    Update spacing and tabs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,47 @@
 module.exports = function(grunt) {
 
-  	grunt.initConfig({
-	  	
-	  	clean: {
+	grunt.initConfig({
+		clean: {
 			build: {
-		    	src: ['_prod']
+				src: ['_prod']
 			}
 		},
-		
-	  	copy: {
-		    build: {
-		        src: [
-		        	'css/style.css',
-		        	'favicon.ico',
-		        	'fonts/*',
-		        	'images/**/*',
-		        	'includes/*.php',
-		        	'js/*.js',
-		        	'*.php'
-		        ],
-		        dest: '_prod',
-		        expand: true
-	    	}
-	    },
-	    
-	    htmlmin: {
+		copy: {
+			build: {
+				src: [
+					'css/style.css',
+					'favicon.ico',
+					'fonts/*',
+					'images/**/*',
+					'includes/*.php',
+					'js/*.js',
+					'*.php'
+				],
+				dest: '_prod',
+				expand: true
+			}
+		},
+		htmlmin: {
 			dist: {
-		    	options: {
-			    	minifyJS: true,
-			    	minifyCSS: true,
+				options: {
+					minifyJS: true,
+					minifyCSS: true,
 					removeComments: true,
 					collapseWhitespace: true
-		    	},
+				},
 				files: [
 					{
 						'_prod/index.php': '_prod/index.php'
-			    	},
-			    	{
-				    	expand: true,
+					},
+					{
+						expand: true,
 						cwd: '_prod/includes',
 						src: '*.php',
 						dest: '_prod/includes'
-			    	}
-		    	]
-		    }
+					}
+				]
+			}
 		},
-		
 		uglify: {
 			build: {
 				files: [
@@ -61,13 +57,12 @@ module.exports = function(grunt) {
 				]
 			}
 		}
-	
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	
+
 	grunt.registerTask('build', ['clean', 'copy', 'htmlmin', 'uglify']);
 };
