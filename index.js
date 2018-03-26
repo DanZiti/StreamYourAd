2b19ca26e3bc6ff41c4ea8e5ea097572d5662341
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2018-03-25 20:31:13 -0600

    Spin off contact api into its own thing

--- index.js
+++ index.js
@@ -1,51 +1,17 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
-
-// @TODO: MOVE NODEMAILER JANK INTO ITS OWN 'routes/api' DIRECTORY
+const apiRoutes = require('./routes/api');
 
 const app = express();
 const port = process.env.port || 8080;
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.post('/api/contact', (req, res) => {
-	const { comments, email, name, subject } = req.body;
-	const transporter = nodemailer.createTransport({
-		host: 'smtp.gmail.com',
-		port: 465,
-		secure: true,
-		auth: {
-			type: 'OAuth2',
-			user: 'dan.zervoudakes@gmail.com',
-			clientId: '286048389250-86g4pd8jcdes2j6sqtuqbq4s4d3vug7v.apps.googleusercontent.com',
-			clientSecret: 'A29tNNT8dvOZK0ezVpOldI-0',
-			refreshToken: '1/2sQWV81orFTA7uIsv4WnCnT-SL1TkUq_vGBnJMrBFpo',
-			accessToken: 'ya29.GluJBWH2KU-3Mvjc4yYG3Md1wRqMxuquRWwz1hiGRlKdULyr0o-e3yLuOL6KZNaT58EAfkkZi8zhs_lOOPTfMgpy7-xGIF9o1A2YlWoAit2GqG4xJ7sQlVpJ2y7W',
-			expires: 1484314697598
-		}
-	});
-	const opts = {
-		from: `${name} <${email}>`,
-		to: 'dan.zervoudakes@gmail.com',
-		subject: `New 'StreamYourAd' inquiry submitted: ${subject}`,
-		html: `
-			<p><span style="font-weight: bold;">Name:</span> ${name}</p>
-			<p><span style="font-weight: bold;">Email:</span> ${email}</p>
-			<p><span style="font-weight: bold;">Comments:</span> ${comments}</p>
-		`
-	};
-	transporter.sendMail(opts, (err, info) => {
-		res.send(err ? err : info);
-	});
-});
+app.use('/api', apiRoutes);
 
 app.use((req, res) => {
 	res.status(404);
---
+++ routes/api/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const router = express.Router();
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.post('/contact', (req, res) => {
+	const { comments, email, name, subject } = req.body;
+	const transporter = nodemailer.createTransport({
+		host: 'smtp.gmail.com',
+		port: 465,
+		secure: true,
+		auth: {
+			type: 'OAuth2',
+			user: 'dan.zervoudakes@gmail.com',
+			clientId: '286048389250-86g4pd8jcdes2j6sqtuqbq4s4d3vug7v.apps.googleusercontent.com',
+			clientSecret: 'A29tNNT8dvOZK0ezVpOldI-0',
+			refreshToken: '1/2sQWV81orFTA7uIsv4WnCnT-SL1TkUq_vGBnJMrBFpo',
+			accessToken: 'ya29.GluJBWH2KU-3Mvjc4yYG3Md1wRqMxuquRWwz1hiGRlKdULyr0o-e3yLuOL6KZNaT58EAfkkZi8zhs_lOOPTfMgpy7-xGIF9o1A2YlWoAit2GqG4xJ7sQlVpJ2y7W',
+			expires: 1484314697598
+		}
+	});
+	const opts = {
+		from: `${name} <${email}>`,
+		to: 'dan.zervoudakes@gmail.com',
+		subject: `New 'StreamYourAd' inquiry submitted: ${subject}`,
+		html: `
+			<p><span style="font-weight: bold;">Name:</span> ${name}</p>
+			<p><span style="font-weight: bold;">Email:</span> ${email}</p>
+			<p><span style="font-weight: bold;">Comments:</span> ${comments}</p>
+		`
+	};
+	transporter.sendMail(opts, (err, info) => {
+		res.send(err ? err : info);
+	});
+});
+
+module.exports = router;
